b8dca019e95f34042b063e1ce1d8bbf5e44b77af
이현석 <geundol222@gmail.com>
2026-01-07 09:06:11 +0900

    [LHS] Update node exports for ControlNet and I2I nodes

---
+++ src/generation/image_generation/nodes/__init__.py
@@ -0,0 +1,16 @@
+"""
+Image Generation Nodes
+"""
+
+from .base import BaseNode
+from .text2image import Text2ImageNode
+from .image2image import Image2ImageControlNetNode
+from .controlnet import ControlNetPreprocessorNode, ControlNetLoaderNode
+
+__all__ = [
+    "BaseNode",
+    "Text2ImageNode",
+    "Image2ImageControlNetNode",
+    "ControlNetPreprocessorNode",
+    "ControlNetLoaderNode",
+]
